1fe2937448cda86deddfb3f5584015b8d15ac79e
Nguyen Phuoc Khanh <khanhnguyen@Nguyens-MacBook-Pro.local>
2021-08-18 18:45:20 +0700

    base

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import Vue2Editor from "vue2-editor";
 
-Vue.config.productionTip = false
+Vue.use(Vue2Editor);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
